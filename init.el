9aeb10ae55c46ae25211e0283df8534ccb19706f
sovereignyy99 <sovereignyy@gmail.com>
2019-11-30 18:52:24 +0800

    add ignore file delete .spacemacs.env

--- init.el
+++ init.el
@@ -36,7 +36,7 @@ values."
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
      ;; <M-m f e R> (Emacs style) to install them.
      ;; ----------------------------------------------------------------
-     
+
      ;; languages layers
      html
      ;; (javascript :variables javascript-backend 'lsp)
@@ -58,7 +58,7 @@ values."
      ;; restructuredtext
      ;; csv
      ;; react
-     
+
 
      ;; tools layers
      ;; sphinx
@@ -100,8 +100,8 @@ values."
      ;; prodigy
      ;; github
      ;; search-engine
-     
-     
+
+
      ;; completion layers
      ;; gtags
      ;; dash ;; open and search docs with Zeal
@@ -115,7 +115,7 @@ values."
                       auto-completion-enable-sort-by-usage t
                       :disabled-for org markdown)
 
-     
+
      ;; better config layers
      better-defaults
      (spacemacs-layouts :variables layouts-enable-autosave nil
@@ -126,7 +126,7 @@ values."
 
      ;; vcs layers
      (git :variables
-          git-magit-status-fullscreen t  
+          git-magit-status-fullscreen t
           magit-push-always-verify nil
           magit-save-repository-buffers 'dontask
           magit-revert-buffers 'silent
@@ -141,7 +141,7 @@ values."
 
      ;; custom packet layers
      ;; sunzhongyang
-     
+
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
@@ -406,18 +406,24 @@ executes.
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
 
+  (message "load user-init...")
+
   (setq configuration-layer--elpa-archives
         '(("melpa-cn" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
           ("org-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
           ("gnu-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
-		  
-  ;;(setq-default configuration-layer-elpa-archives
-   ;;              '(("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
-   ;;                ("org-cn"   . "http://elpa.emacs-china.org/org/")
 
+  ;;(setq-default configuration-layer-elpa-archives
+  ;;              '(("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
+  ;;                ("org-cn"   . "http://elpa.emacs-china.org/org/")
   ;;                ("gnu-cn"   . "http://elpa.emacs-china.org/gnu/")))
 
   ;; (setq term-char-mode-point-at-process-mark nil)
+
+  (setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
+  (load custom-file)
+  ;; (load (expand-file-name "init-syntax-table.el" dotspacemacs-directory))
+  ;; (load (expand-file-name "xcwen-misc.el" dotspacemacs-directory))
 
   )
 
@@ -429,6 +435,8 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
 
+  (message "load user-config...")
+
   (require 'beacon)
   (beacon-mode t)
 
@@ -438,30 +446,30 @@ you should place your code here."
   (require 'dired-x)
 
   ;; Appointments and notifications
-;;  (require 'notifications)
-;;  (require 'appt)
-;;  (defun appt-agenda-notify (minutes-to-appt time-current message)
-;;    "Display a notification before scheduled events registered in org-agenda"
-;;    (notifications-notify :title "Appointment"
-;;                          :body (format "In %s minutes:" minutes-to-appt) message
-;;                          :app-name "Emacs: Org"
-;;                          :urgency "normal"
-;;                          :sound-name "alarm-clock-elapsed")
-;;    )
-;;  (setq appt-message-warning-time 15
-;;        appt-display-mode-line t
-;;        appt-display-format 'window
-;;        appt-disp-window-function (function appt-agenda-notify))
+  ;;  (require 'notifications)
+  ;;  (require 'appt)
+  ;;  (defun appt-agenda-notify (minutes-to-appt time-current message)
+  ;;    "Display a notification before scheduled events registered in org-agenda"
+  ;;    (notifications-notify :title "Appointment"
+  ;;                          :body (format "In %s minutes:" minutes-to-appt) message
+  ;;                          :app-name "Emacs: Org"
+  ;;                          :urgency "normal"
+  ;;                          :sound-name "alarm-clock-elapsed")
+  ;;    )
+  ;;  (setq appt-message-warning-time 15
+  ;;        appt-display-mode-line t
+  ;;        appt-display-format 'window
+  ;;        appt-disp-window-function (function appt-agenda-notify))
 
   ;; Update appointments automatically
-;;  (org-agenda-to-appt)
-;;  (run-at-time "12:00am" (* 24 3600) 'org-agenda-to-appt)
-;;  (add-hook 'after-save-hook
-;;            '(lambda ()
-;;               (if (seq-contains (org-agenda-files) (buffer-file-name))
-;;                   (org-agenda-to-appt))))
-;;  (appt-activate 1)
-;;  (display-time)
+  ;;  (org-agenda-to-appt)
+  ;;  (run-at-time "12:00am" (* 24 3600) 'org-agenda-to-appt)
+  ;;  (add-hook 'after-save-hook
+  ;;            '(lambda ()
+  ;;               (if (seq-contains (org-agenda-files) (buffer-file-name))
+  ;;                   (org-agenda-to-appt))))
+  ;;  (appt-activate 1)
+  ;;  (display-time)
 
   ;; 解决org表格中英文对齐的问题
   (when (configuration-layer/layer-usedp 'chinese)
@@ -469,19 +477,19 @@ you should place your code here."
       (spacemacs//set-monospaced-font "Source Code Pro" "Microsoft YaHei" 14 16)))
 
   ;; Org mode settings
-;;  (setq org-directory "~/mydocs/org/")
-;;  (setq org-default-notes-file (concat org-directory "tasks.org"))
-;;  (setq org-todo-keywords '((sequence "TODO" "PROGRESS" "|" "DONE" "DELEGATED" "CANCELLED")))
-;;  (setq org-refile-targets
-;;        '((nil :maxlevel . 1)
-;;          (org-agenda-files :maxlevel . 1)))
+  ;;  (setq org-directory "~/mydocs/org/")
+  ;;  (setq org-default-notes-file (concat org-directory "tasks.org"))
+  ;;  (setq org-todo-keywords '((sequence "TODO" "PROGRESS" "|" "DONE" "DELEGATED" "CANCELLED")))
+  ;;  (setq org-refile-targets
+  ;;        '((nil :maxlevel . 1)
+  ;;          (org-agenda-files :maxlevel . 1)))
 
   (setq company-show-numbers t)
   (setq flycheck-check-syntax-automatically '(new-line save))
   (custom-set-faces
    '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
    '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil)))))
-  
+
   ;; Major mode associations
   ;; (add-to-list 'auto-mode-alist '("\\.tscn\\'" . toml-mode))
   (setq spacemacs-large-file-modes-list '(archive-mode tar-mode jka-compr git-commit-mode image-mode doc-view-mode doc-view-mode-maybe ebrowse-tree-mode pdf-view-mode fundamental-mode ggtags-mode helm-gtags-mode tags-table-mode))
@@ -521,135 +529,9 @@ you should place your code here."
   ;; (spacemacs|diminish counsel-mode)
   )
 
-
-;; Do not write anything past this comment. This is where Emacs will
-;; auto-generate custom variable definitions.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (xterm-color ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package unfill toc-org spaceline powerline smeargle shell-pop restart-emacs rainbow-delimiters popwin persp-mode pcre2el paradox spinner orgit org-projectile org-category-capture org-present org-pomodoro alert log4e gntp org-plus-contrib org-mime org-download org-bullets open-junk-file neotree mwim multi-term move-text mmm-mode markdown-toc markdown-mode magit-gitflow magit-popup macrostep lorem-ipsum linum-relative link-hint indent-guide hydra lv hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation helm-themes helm-swoop helm-projectile projectile pkg-info epl helm-mode-manager helm-make helm-gitignore request helm-flx helm-descbinds helm-company helm-c-yasnippet helm-ag google-translate golden-ratio gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md fuzzy flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist highlight evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit magit transient git-commit with-editor evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu eshell-z eshell-prompt-extras esh-help elisp-slime-nav dumb-jump f dash s diminish define-word company-statistics company column-enforce-mode clean-aindent-mode bind-map bind-key auto-yasnippet yasnippet auto-highlight-symbol auto-compile packed aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core async ac-ispell auto-complete popup))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 (defun dotspacemacs/emacs-custom-settings ()
   "Emacs custom settings.
 This is an auto-generated function, do not modify its content directly, use
 Emacs customize menu instead.
 This function is called at the very end of Spacemacs initialization."
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(company-quickhelp-color-background "#4F4F4F")
- '(company-quickhelp-color-foreground "#DCDCCC")
- '(compilation-message-face (quote default))
- '(cua-global-mark-cursor-color "#2aa198")
- '(cua-normal-cursor-color "#839496")
- '(cua-overwrite-cursor-color "#b58900")
- '(cua-read-only-cursor-color "#859900")
- '(custom-safe-themes
-   (quote
-    ("834cbeacb6837f3ddca4a1a7b19b1af3834f36a701e8b15b628cad3d85c970ff" "a2cde79e4cc8dc9a03e7d9a42fabf8928720d420034b66aecc5b665bbf05d4e9" default)))
- '(evil-want-Y-yank-to-eol nil)
- '(fci-rule-color "#073642")
- '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
- '(highlight-symbol-colors
-   (--map
-    (solarized-color-blend it "#002b36" 0.25)
-    (quote
-     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
- '(highlight-symbol-foreground-color "#93a1a1")
- '(highlight-tail-colors
-   (quote
-    (("#073642" . 0)
-     ("#5b7300" . 20)
-     ("#007d76" . 30)
-     ("#0061a8" . 50)
-     ("#866300" . 60)
-     ("#992700" . 70)
-     ("#a00559" . 85)
-     ("#073642" . 100))))
- '(hl-bg-colors
-   (quote
-    ("#866300" "#992700" "#a7020a" "#a00559" "#243e9b" "#0061a8" "#007d76" "#5b7300")))
- '(hl-fg-colors
-   (quote
-    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
- '(hl-todo-keyword-faces
-   (quote
-    (("HOLD" . "#d0bf8f")
-     ("TODO" . "#dc752f")
-     ("NEXT" . "#dc752f")
-     ("THEM" . "#dc8cc3")
-     ("PROG" . "#4f97d7")
-     ("OKAY" . "#4f97d7")
-     ("DONT" . "#f2241f")
-     ("FAIL" . "#f2241f")
-     ("DONE" . "#86dc2f")
-     ("NOTE" . "#b1951d")
-     ("KLUDGE" . "#d0bf8f")
-     ("HACK" . "#b1951d")
-     ("TEMP" . "#b1951d")
-     ("FIXME" . "#dc752f")
-     ("XXX+" . "#cc9393"))))
- '(magit-diff-use-overlays nil)
- '(nrepl-message-colors
-   (quote
-    ("#dc322f" "#cb4b16" "#b58900" "#5b7300" "#b3c34d" "#0061a8" "#2aa198" "#d33682" "#6c71c4")))
- '(package-selected-packages
-   (quote
-    (xterm-color ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package unfill toc-org spaceline powerline smeargle shell-pop restart-emacs rainbow-delimiters popwin persp-mode pcre2el paradox spinner orgit org-projectile org-category-capture org-present org-pomodoro alert log4e gntp org-plus-contrib org-mime org-download org-bullets open-junk-file neotree mwim multi-term move-text mmm-mode markdown-toc markdown-mode magit-gitflow magit-popup macrostep lorem-ipsum linum-relative link-hint indent-guide hydra lv hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation helm-themes helm-swoop helm-projectile projectile pkg-info epl helm-mode-manager helm-make helm-gitignore request helm-flx helm-descbinds helm-company helm-c-yasnippet helm-ag google-translate golden-ratio gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md fuzzy flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist highlight evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit magit transient git-commit with-editor evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu eshell-z eshell-prompt-extras esh-help elisp-slime-nav dumb-jump f dash s diminish define-word company-statistics company column-enforce-mode clean-aindent-mode bind-map bind-key auto-yasnippet yasnippet auto-highlight-symbol auto-compile packed aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core async ac-ispell auto-complete popup)))
- '(pdf-view-midnight-colors (quote ("#b2b2b2" . "#292b2e")))
- '(pos-tip-background-color "#073642")
- '(pos-tip-foreground-color "#93a1a1")
- '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
- '(term-default-bg-color "#002b36")
- '(term-default-fg-color "#839496")
- '(vc-annotate-background nil)
- '(vc-annotate-background-mode nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#dc322f")
-     (40 . "#cb4366eb20b4")
-     (60 . "#c1167942154f")
-     (80 . "#b58900")
-     (100 . "#a6ae8f7c0000")
-     (120 . "#9ed892380000")
-     (140 . "#96be94cf0000")
-     (160 . "#8e5397440000")
-     (180 . "#859900")
-     (200 . "#77679bfc4635")
-     (220 . "#6d449d465bfd")
-     (240 . "#5fc09ea47092")
-     (260 . "#4c68a01784aa")
-     (280 . "#2aa198")
-     (300 . "#303498e7affc")
-     (320 . "#2fa1947cbb9b")
-     (340 . "#2c879008c736")
-     (360 . "#268bd2"))))
- '(vc-annotate-very-old-color nil)
- '(weechat-color-list
-   (quote
-    (unspecified "#002b36" "#073642" "#a7020a" "#dc322f" "#5b7300" "#859900" "#866300" "#b58900" "#0061a8" "#268bd2" "#a00559" "#d33682" "#007d76" "#2aa198" "#839496" "#657b83")))
- '(xterm-color-names
-   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
- '(xterm-color-names-bright
-   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
- '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil)))))
 )
